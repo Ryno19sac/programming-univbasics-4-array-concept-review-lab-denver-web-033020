2033fc5fb470998bbead03ebe620d81ef00a9550
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-11 04:45:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,8 +10,7 @@ end
 
 def find_max_value(array)
   x = array[0]
-  array.length.times  {|index|
-  if array[index] > x 
+  array.length.times  {|index| if array[index] > x 
     x = array[index]
   end
   }
